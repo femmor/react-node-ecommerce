4f79ad3695d824fa7c9645a007693a92efcca3ee
Emmanuel Egomson <femmor2u@gmail.com>
2020-06-03 15:37:11 -0500

    created the product model and product adding functionality

---
+++ backend/models/productModel.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+const productModel = mongoose.model("Product", productSchema) 
+
+export default productModel;
+
+
+
+
